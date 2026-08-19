1667dd76d5df90c7eae90946d988129553629adc
V Monish Kumar <monish28kumar@gmail.com>
2026-08-19 21:43:50 +0530

    Time: 0 ms (100%), Space: 9.7 MB (90.94%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int i = 0, j = 0, star = -1, match = 0;
+        while (i < s.size()) {
+            if (j < p.size() && (p[j] == '?' || p[j] == s[i])) {
+                i++; j++;
+            } else if (j < p.size() && p[j] == '*') {
+                star = j++;
+                match = i;
+            } else if (star != -1) {
+                j = star + 1;
+                i = ++match;
+            } else {
+                return false;
+            }
+        }
+        while (j < p.size() && p[j] == '*') j++;
+        return j == p.size();
+    }
+};
