b431ce031431f0a709a55e92bc75ec1a5629ea05
V Monish Kumar <monish28kumar@gmail.com>
2026-09-16 21:50:25 +0530

    Time: 11 ms (0.4%), Space: 12.7 MB (12.74%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        vector<int> ans;
+        for(int i=0;i<n;i++) {
+            sum += nums[i];
+            ans.push_back(sum);
+        }
+
+        for(auto it: ans) {
+            cout << it;
+        }
+
+        return ans;
+        
+    }
+};
